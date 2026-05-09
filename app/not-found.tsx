f0eb038e44e284eaf056a767020b1f3c94a64c40
Artem Chupylko <48257625+achupylko@users.noreply.github.com>
2026-05-09 21:47:23 +0300

    feat(meta): add metadata support (#1)

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,4 +1,31 @@
+import { Metadata } from 'next';
 import css from './page.module.css';
+
+export const metadata: Metadata = {
+  title: 'Not Found',
+  description: 'Page not found.',
+  openGraph: {
+    title: 'Not Found',
+    description: 'Page not found.',
+    url: 'https://notehub.com/',
+    siteName: 'NoteHub',
+    images: [
+      {
+        url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+        width: 1200,
+        height: 630,
+        alt: 'NoteHub preview',
+      },
+    ],
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Not Found',
+    description: 'Page not found.',
+    images: ['https://ac.goit.global/fullstack/react/notehub-og-meta.jpg'],
+  },
+};
 
 export default function NotFound() {
   return (
--- app/notes/filter/[...slug]/page.tsx
+++ app/notes/filter/[...slug]/page.tsx
@@ -5,10 +5,43 @@ import {
   HydrationBoundary,
   QueryClient,
 } from '@tanstack/react-query';
+import { Metadata } from 'next';
 
 type Props = {
   params: Promise<{ slug: string[] }>;
 };
+
+export async function generateMetadata({ params }: Props): Promise<Metadata> {
+  const { slug } = await params;
+
+  const category = slug[0];
+
+  return {
+    title: category,
+    description: `Notes from the ${category} category`,
+    openGraph: {
+      title: category,
+      description: `Notes from the ${category} category`,
+      url: 'https://notehub.com/',
+      siteName: 'NoteHub',
+      images: [
+        {
+          url: 'https://ac.goit.global/fullstack/react/notehub-og-meta.jpg',
+          width: 1200,
+          height: 630,
+          alt: 'NoteHub preview',
+        },
+      ],
+      type: 'website',
+    },
+    twitter: {
+      card: 'summary_large_image',
+      title: category,
+      description: `Notes from the ${category} category`,
+      images: ['https://ac.goit.global/fullstack/react/notehub-og-meta.jpg'],
+    },
+  };
+}
 
 export default async function NotesByCategory({ params }: Props) {
   const { slug } = await params;
